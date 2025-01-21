f69aebcdbb8bba7f53197f0095caf68a6e8e33d0
GhiggiNicolas <nicolas.ghiggi@samtrevano.ch>
2025-01-21 22:38:06 +0100

    Updated global CSS and Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
 "use client"
 
+import { ModeToggle } from "@/components/ModeToggle"
+
 const Home = () => {
-    return <div>Hello World</div>
+    return (
+        <div className="w-full h-screen flex items-center justify-center gap-2">
+            Hello World <ModeToggle/>
+        </div>
+    );
 }
 
 export default Home
